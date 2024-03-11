c270cb6a981d68c14ee57dc515c8fbf8759099db
Dr Nilo Stolte <80269251+nilostolte@users.noreply.github.com>
2024-03-11 11:15:36 -0400

    Add files via upload

--- Zig/sudoku.zig
+++ Zig/sudoku.zig
@@ -15,14 +15,14 @@ pub fn main() !void {
        grid.set(args[1]);
     }
     else {
-        grid.set("800000000003600000070090200050007000000045700000100030001000068008500010090000400");
+       grid.set("800000000003600000070090200050007000000045700000100030001000068008500010090000400");
     }
-	std.debug.print("\n===================\n    Input Grid\n===================\n", .{});
-	grid.print();
-	var timer = try std.time.Timer.start();
-	grid.solve();
+    std.debug.print("\n===================\n    Input Grid\n===================\n", .{});
+    grid.print();
+    var timer = try std.time.Timer.start();
+    grid.solve();
     const time_0 : u64 = timer.read();
-	std.debug.print("\n===================\n     Solution\n===================\n", .{});
+    std.debug.print("\n===================\n     Solution\n===================\n", .{});
     grid.print();
     @setFloatMode(.Optimized);
     const ftime =  @as(f64,@floatFromInt(time_0))/1000000.0;
